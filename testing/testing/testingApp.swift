5b73253e5f8b8a2c80987185865f138e4a35b6cd
Kauri0000 <kauri.adeduro@hgs.hiddengeniusproject.org>
2026-06-15 14:29:28 -0700

    first test thing
    created x code project

---
+++ testing/testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Kauri Adeduro on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
